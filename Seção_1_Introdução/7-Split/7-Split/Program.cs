565205a247bcb93a96c8db1bf4ca6ddbb497ea1c
Emerson Oliveira <emerson.520@outlook.com>
2023-08-27 18:59:31 -0300

    Aprendendo entrada de dados

---
+++ Seção_1_Introdução/7-Split/7-Split/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string cores = Console.ReadLine();
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string cor1 = vet[0];
+            string cor2 = vet[1];
+            string cor3 = vet[2];
+
+            Console.WriteLine(cor1);
+            Console.WriteLine(cor2);
+            Console.WriteLine(cor3);
+        }
+    }
+}
