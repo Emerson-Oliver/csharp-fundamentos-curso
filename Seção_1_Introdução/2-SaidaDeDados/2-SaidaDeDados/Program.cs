cac778fa3f64a2cc0d187e95d4fcffb020ce4304
Emerson Oliveira <emerson.520@outlook.com>
2023-08-26 22:18:20 -0300

    Aprendendo Saida de Dados

---
+++ Seção_1_Introdução/2-SaidaDeDados/2-SaidaDeDados/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 10.123456;
+            string nome = "Emerson";            
+            int idade = 38;
+            char sexo = 'M';
+            
+            Console.WriteLine("Ola, Mundo");
+            Console.Write("Bom dia_");
+            Console.Write("Boa Tarde");            
+            Console.WriteLine();
+            Console.WriteLine(numero);
+            Console.WriteLine(numero.ToString("F4"));
+            Console.WriteLine(numero.ToString("F2"));
+            Console.WriteLine(numero.ToString("F4",CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine($"O Funcionario {nome} tem {idade} anos e é do sexo {sexo}"); // Interpolação
+            Console.WriteLine("O Funcionario " + nome + " tem " + idade + " anos e é do sexo " + sexo); // Concatenação
+            Console.ReadLine();
+        }
+    }
+}
