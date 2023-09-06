34050da007954148040332e627229aa15e623849
Emerson Oliveira <emerson.520@outlook.com>
2023-09-05 21:38:31 -0300

    Add: segunda parte dos exercicios sobre estrutura condicional

---
+++ Exercicios_Seção_2-Estrutura_Condicional/Bee1040/Bee1040/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bee1040
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] notas = Console.ReadLine().Split(' ');
+            float N1 = float.Parse(notas[0],CultureInfo.InvariantCulture);
+            float N2 = float.Parse(notas[1],CultureInfo.InvariantCulture);
+            float N3 = float.Parse(notas[2],CultureInfo.InvariantCulture);
+            float N4 = float.Parse(notas[3],CultureInfo.InvariantCulture);
+
+            float media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10;
+            Console.WriteLine("Media: " +media.ToString("F1",CultureInfo.InvariantCulture));
+            if(media >= 7f)
+            {
+                Console.WriteLine("Aluno aprovado"); 
+            }
+            else if (media < 5f)
+            {
+                Console.WriteLine("Aluno Reprovado");
+            }
+            else if (media >= 5f && media <= 6.9f)
+            {
+                Console.WriteLine("Aluno em exame.");
+                float notaExame = float.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.WriteLine("Nota do exame: " + notaExame);
+                float mediaFinal = (media + notaExame) / 2f;
+                if(mediaFinal >= 5f)
+                {
+                    Console.WriteLine("Aluno Aprovado");
+                    Console.WriteLine("Media Final: " +mediaFinal);
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado");
+                }
+            }
+        }
+    }
+}
---
+++ Exercicios_Seção_2-Estrutura_Condicional/Bee1048/Bee1048/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bee1048
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float percentual = 0;
+            double aumento = 0;
+            double novoSalario = 0;
+            double salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            if (salario <= 400.00)
+            {
+                novoSalario = salario / 100 * 115.0;
+                aumento = novoSalario - salario;
+                percentual = 15f;
+            }
+            else if (salario >= 400.00 && salario <= 800.00)
+            {
+                novoSalario = salario / 100 * 112.0;
+                aumento = novoSalario - salario;
+                percentual = 12f;
+            }
+            else if (salario >= 800.01 && salario <= 1200.00)
+            {
+                novoSalario = salario / 100 * 110.0;
+                aumento = novoSalario - salario;
+                percentual = 10f;
+            }
+            else if (salario >= 1200.01 && salario <= 2000.00)
+            {
+                novoSalario = salario / 100 * 107.0;
+                aumento = novoSalario - salario;
+                percentual = 7f;
+            }
+            else
+            {
+                novoSalario = salario / 100 * 104.0;
+                aumento = novoSalario - salario;
+                percentual = 4f;
+            }
+            Console.WriteLine("Salario: " + novoSalario.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + aumento.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + percentual + " %");
+        }
+    }
+}
