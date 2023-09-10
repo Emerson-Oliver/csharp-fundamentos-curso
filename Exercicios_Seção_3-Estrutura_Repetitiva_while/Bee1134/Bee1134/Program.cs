402c3aadc2081376a105651be7072260893e8b56
Emerson Oliveira <emerson.520@outlook.com>
2023-09-10 00:03:16 -0300

    Primeiros exercicios estrutura while

---
+++ Exercicios_Seção_3-Estrutura_Repetitiva_while/Bee1134/Bee1134/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bee1134
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            int tipoCombustivel = int.Parse(Console.ReadLine());
+
+            while (tipoCombustivel != 4)
+            {
+               if (tipoCombustivel == 1)
+                {
+                    alcool = alcool + 1;
+                }
+               else if (tipoCombustivel == 2)
+                {
+                    gasolina = gasolina + 1;
+                }
+               else if (tipoCombustivel == 3)
+                {
+                    diesel = diesel + 1;
+                }             
+
+                tipoCombustivel = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
---
+++ Exercicios_Seção_3-Estrutura_Repetitiva_while/Bee1154/Bee1154/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace Bee1154
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            float soma = 0, contagem = 0;
+            while(idade >= 0)
+            {
+                contagem = contagem + 1;
+                soma = soma + idade;
+                idade = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+            double idadeMedia = (double) soma / contagem;
+            Console.WriteLine(idadeMedia.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
