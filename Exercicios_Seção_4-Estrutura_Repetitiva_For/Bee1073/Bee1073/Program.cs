ed68be99120336b3851481982a20ada02f64c087
Emerson Oliveira <emerson.520@outlook.com>
2023-09-15 03:22:45 -0300

    Add: ultimos exercicio estrutura for

---
+++ Exercicios_Seção_4-Estrutura_Repetitiva_For/Bee1073/Bee1073/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bee1073
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int index = 2; index <= N; index += 1)
+            {
+                if(index % 2 == 0)
+                {
+                    int quadrado = index * index;
+                    Console.WriteLine(index + "^2 = " + quadrado);
+                }
+            }
+        }
+    }
+}
---
+++ Exercicios_Seção_4-Estrutura_Repetitiva_For/Bee1075/Bee1075/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bee1075
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for(int index = 1; index <=1000; index += 1)
+            {
+                if(index % N == 2)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+        }
+    }
+}
